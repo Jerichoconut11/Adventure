933fb54c00d75ee8fa4f8d8b7e18dc6cc64646b3
Jerichoconut11 <lumabajericho11@gmail.com>
2025-08-06 21:42:03 +0800

    Updating Folder naming's

---
+++ StudyTracker/practice.js
@@ -0,0 +1,22 @@
+const subsInput = document.querySelector(".subject-input");
+const durationInput = document.querySelector(".duration-input");
+const subsOutput = document.querySelector(".subject-output");
+const durationOuput = document.querySelector(".duration-output");
+
+const buttons = document.querySelectorAll("button");
+let studyArr = [];
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.currentTarget.id === "reset") return resetAllSessions{
+    }
+  });
+});
+
+function addSession() {
+
+}
+
+function resetAllSessions(){
+
+}
